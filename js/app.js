807678f96e06e5f91fe6a937dde15ecd02722b6f
Bassel07 <basel.rizeq93@gmail.com>
2021-06-22 11:35:04 -0700

    added line

--- js/app.js
+++ js/app.js
@@ -22,3 +22,64 @@ switch (answer1){
            break; 
 }
 
+let answer2 = prompt ("Can you make yourself disappear? yes/no").toUpperCase();
+
+switch (answer2){
+    case 'yes':
+        console.log('correct')
+        break;
+    case 'no':
+        console.log('correct')   
+        break;
+        
+       default:
+           console.log('wrong answer')
+           break; 
+}
+
+let answer3 = prompt ("Can you see out the back of your head? yes/no").toUpperCase();
+
+switch (answer3){
+    case 'yes':
+        console.log('correct')
+        break;
+    case 'no':
+        console.log('correct')   
+        break;
+        
+       default:
+           console.log('wrong answer')
+           break; 
+}
+
+let answer4 = prompt ("Were you born in the summer? yes/no").toUpperCase();
+
+switch (answer4){
+    case 'yes':
+        console.log('correct')
+        break;
+    case 'no':
+        console.log('correct')   
+        break;
+        
+       default:
+           console.log('wrong answer')
+           break; 
+}
+
+let answer5 = prompt ("Do you know how to swim? yes/no").toUpperCase();
+
+switch (answer5){
+    case 'yes':
+        console.log('correct')
+        break;
+    case 'no':
+        console.log('correct')   
+        break;
+        
+       default:
+           console.log('wrong answer')
+           break; 
+}
+
+
