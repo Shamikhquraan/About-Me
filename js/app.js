796002d4cff04f885c162a2bd84cb50a0f9ca7c2
Bassel07 <basel.rizeq93@gmail.com>
2021-06-22 11:41:16 -0700

    change line

--- js/app.js
+++ js/app.js
@@ -12,14 +12,14 @@ let answer1 = prompt ("Do you believe in ghosts? yes/no").toUpperCase();
 switch (answer1){
     case 'yes':
         console.log('correct')
-        break;
+        
     case 'no':
         console.log('correct')   
-        break;
+        
         
        default:
            console.log('wrong answer')
-           break; 
+           
 }
 
 let answer2 = prompt ("Can you make yourself disappear? yes/no").toUpperCase();
@@ -27,14 +27,14 @@ let answer2 = prompt ("Can you make yourself disappear? yes/no").toUpperCase();
 switch (answer2){
     case 'yes':
         console.log('correct')
-        break;
+        
     case 'no':
         console.log('correct')   
-        break;
+        
         
        default:
            console.log('wrong answer')
-           break; 
+           
 }
 
 let answer3 = prompt ("Can you see out the back of your head? yes/no").toUpperCase();
@@ -42,14 +42,14 @@ let answer3 = prompt ("Can you see out the back of your head? yes/no").toUpperCa
 switch (answer3){
     case 'yes':
         console.log('correct')
-        break;
+        
     case 'no':
         console.log('correct')   
-        break;
+        
         
        default:
            console.log('wrong answer')
-           break; 
+           
 }
 
 let answer4 = prompt ("Were you born in the summer? yes/no").toUpperCase();
@@ -57,14 +57,14 @@ let answer4 = prompt ("Were you born in the summer? yes/no").toUpperCase();
 switch (answer4){
     case 'yes':
         console.log('correct')
-        break;
+        
     case 'no':
         console.log('correct')   
-        break;
+        
         
        default:
            console.log('wrong answer')
-           break; 
+           
 }
 
 let answer5 = prompt ("Do you know how to swim? yes/no").toUpperCase();
